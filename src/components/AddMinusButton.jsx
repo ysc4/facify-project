86273a9f7355049bafbeac73993a96b026948695
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-01-11 12:02:09 +0800

    Created counter buttons

---
+++ src/components/AddMinusButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useState } from 'react';
+import '../App.css';
+import './AddMinusButton.css';
+
+function AddMinusButton() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+    const decrement = () => {
+        setCount(count - 1);
+    }
+
+    return (
+        <div className="AddMinusButton">
+            <span className="minus" onClick={decrement}>-</span>
+            <span className="count">{count}</span>
+            <span className="plus" onClick={increment}>+</span>
+        </div>
+    );
+}
+
+export default AddMinusButton;
