07c98fcd339f3b8090fbbe026abaf6dc8df1161c
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-01-22 18:04:32 +0800

    Changed routing logic of pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Booking from './pages/Booking-Home';
 import BookingInfo from './pages/Booking-Info';
 import Login from './pages/Login';
 import Venue from './pages/Venue-Availability';
 import VenueBooking from './pages/Venue-Booking';
 
-export default function  App() {
+function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  return (
-    <Router>
-      <div className='App'>
-        <Routes>
-          <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path="/bookings" element={<Booking />} />
-          <Route path="/booking-info" element={<BookingInfo />} />
-          <Route path="/venue-availability" element={<Venue />} />
-          <Route path="/venue-booking" element={<VenueBooking />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+
+  const NotFound = () => <h1>404 Not Found</h1>;
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Login setIsLoggedIn={setIsLoggedIn} />,
+      errorElement: <NotFound />
+    },
+    {
+      path: '/bookings',
+      element: <Booking />,
+      errorElement: <NotFound />
+    },
+    {
+      path: '/booking-info',
+      element: <BookingInfo />,
+      errorElement: <NotFound />
+    },
+    {
+      path: '/venue-availability',
+      element: <Venue />,
+      errorElement: <NotFound />
+    },
+    {
+      path: '/venue-booking',
+      element: <VenueBooking />,
+      errorElement: <NotFound />
+    }
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
