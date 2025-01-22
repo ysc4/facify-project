e4940c6b50b1520f38d1523762f68d7b74efe3c7
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-01-22 18:14:05 +0800

    Changed from submit requirements to cancel booking

--- src/pages/Booking-Info.jsx
+++ src/pages/Booking-Info.jsx
@@ -1,10 +1,13 @@
 import BackIcon from '@mui/icons-material/ArrowBackOutlined';
+import { Link } from 'react-router-dom';
 import '../components/Navbar.css';
 import Header from '../components/Navbar.jsx';
 import '../components/ProgressBar.css';
+import ProgressBar from '../components/ProgressBar.jsx';
 import '../components/Sidebar.css';
 import Sidebar from '../components/Sidebar.jsx';
 import './Booking-Info.css';
+
 
 function BookingInfo() {
   return (
@@ -21,12 +24,15 @@ function BookingInfo() {
                     <h2>Booking Information - FACI0001</h2>
                 </div>
                 <div className="booking-buttons">
-                    <button className="submit-button">Submit Requirements</button>
-                    <button className="edit-button">Edit Submission</button>
+                    <button className="cancel-button">Cancel Booking</button>
+                    <Link to="/venue-booking">
+                      <button className="edit-button">Edit Submission</button>
+                    </Link>
                 </div>
             </div>
             <div className="booking-progress">
               <div className="progress-bar">
+                <ProgressBar />
               </div>
             </div>
             <div className="event-info">
@@ -87,8 +93,55 @@ function BookingInfo() {
               </div>
             </div>
           </div>
-          <div className="cancel-booking">
-            <button className="cancel-button">Cancel Booking</button>
+          <div className="submit-reqs">
+            <h3>Submitted Requirements</h3>
+            <div className="reqs">
+              <div className="table-header">
+                <div className="req-title">Requirement</div>
+                <div className="date-title">Date Submitted</div>
+                <div className="file-title">File</div>
+              </div>
+              <div className="table-content">
+                <div className="req">
+                  <div className="file-name">Letter of Intent</div>
+                  <div className="date">January 05, 2024</div>
+                  <div className="file">View</div>
+                </div>
+                <div className="req">
+                  <div className="file-name">Event Proposal</div>
+                  <div className="date">January 05, 2024</div>
+                  <div className="file">
+                    <div className="file-icon">
+                      <span>P</span>
+                    </div>
+                    <div className="file-item">
+                      <h4>PDF File Name</h4>
+                      <p>177.89 KB</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="req">
+                  <div className="file-name">Activity Request Form</div>
+                  <div className="date">January 05, 2024</div>
+                  <div className="file">
+                    <button className="upload-button">Upload PDF File</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="update-logs">
+            <h3>Update Logs</h3>
+            <div className="logs">
+              <div className="log">
+                <div className="log-entry">
+                  <p>Booking was created. <b>FACI0001</b> by ORGANIZATION NAME</p>
+                </div>
+                <div className="log-date">
+                  <p>January 05, 2024 11:00AM</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
