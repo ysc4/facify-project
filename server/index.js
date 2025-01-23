a72eecfe86608776aeb3d2e7b894edadd560e814
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-01-23 23:12:42 +0800

    Added backend for displaying information each booking item

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import mysql from 'mysql';
@@ -6,6 +7,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 
 const app = express();
 const db = mysql.createConnection({
@@ -19,6 +21,7 @@ const db = mysql.createConnection({
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3001; // Change the port number here
 
@@ -43,9 +46,25 @@ app.post('/facify/login', (req, res) => {
             return res.status(500).send({ success: false, message: 'Database error' });
         }
         if (result.length > 0) {
-            res.send({ success: true });
+            const user = result[0];
+            res.send({ success: true, org_id: user.org_id });
         } else {
             res.send({ success: false, message: 'Invalid credentials' });
         }
+    });
+});
+
+app.post('/facify/bookings/:orgID', (req, res) => {
+    const { orgID } = req.params;
+    if (!orgID) {
+        return res.status(400).send({ success: false, message: 'No bookings submitted' });
+    }
+    const query = 'SELECT event_information.booking_id, event_information.event_date, event_information.event_start, event_information.event_end, event_information.activity_title, facilities.facility_name, status.status_name, user.org_name FROM event_information JOIN facilities ON event_information.facility_id = facilities.facility_id JOIN booking_status ON event_information.booking_id = booking_status.booking_id JOIN status ON booking_status.status_id = status.status_id JOIN user ON event_information.org_id = user.org_id WHERE event_information.org_id = ?';
+    db.query(query, [orgID], (err, result) => {
+        if (err) {
+            console.error('Database query error:', err);
+            return res.status(500).send({ success: false, message: 'Database error' });
+        }
+        res.send({ success: true, bookings: result });
     });
 });
