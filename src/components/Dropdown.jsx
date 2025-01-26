22d4a5f097494f16796c16326358981d9538dd10
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-01-26 14:58:42 +0800

    Updated dropdown for filtering logic

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,17 +2,19 @@ import React, { useState } from 'react';
 import './Dropdown.css';
 import Select from './Select';
 
-function Dropdown({options}) {
+function Dropdown({ options, onSelect }) {
     const [menuShow, setMenuShow] = useState(false)
-    const [selected, setSelected] = useState(options[0])
+    const [selected, setSelected] = useState('All Facilities')
 
     const selectOption = e => {
-        setSelected(e.target.innerText);
+        const selectedOption = e.target.innerText;
+        setSelected(selectedOption);
         setMenuShow(!menuShow);
-    }
+        onSelect(selectedOption);
+    };
 
-    const dropdownList = options.map((option,index) =>
-        <li key={index} onClick={selectOption}>{option}</li> )
+    const dropdownList = options.map((option,index) => (
+        <li key={index} onClick={selectOption}>{option}</li> ));
 
     return (
         <div className="dropdown">
