e9fbdc29f3f7ec90dbf1a738d21e5ae47b417deb
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-02-19 19:50:53 +0800

    Added input functionality

--- src/pages/Venue-Booking.jsx
+++ src/pages/Venue-Booking.jsx
@@ -1,4 +1,7 @@
 import BackIcon from '@mui/icons-material/ArrowBackOutlined';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import '../components/AddMinusButton.css';
 import AddMinusButton from '../components/AddMinusButton.jsx';
 import '../components/Navbar.css';
@@ -8,6 +11,99 @@ import Sidebar from '../components/Sidebar.jsx';
 import './Venue-Booking.css';
 
 function VenueBooking() {
+  const { orgID, facilityID } = useParams();
+  const navigate = useNavigate();
+  const [status, setStatus] = useState("");
+  const [formData, setFormData] = useState({
+    orgID: orgID,
+    eventDate: "",
+    eventStart: "",
+    eventEnd: "",
+    activityTitle: "",
+    facilityID: facilityID || "",
+    attendance: "",
+    speakerName: "",
+    equipment: {
+      tables: 0,
+      chairs: 0,
+      bulletinBoards: 0,
+      speaker: 0,
+      microphone: 0,
+      flagpole: 0,
+      podium: 0,
+      platform: 0,
+      electrician: 0,
+      janitor: 0
+    },
+    status: status,
+    bookingDate: new Date().toISOString().split('T')[0],
+    bookingTime: new Date().toISOString().split('T')[1].split('.')[0]
+  });
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleStatusChange = (e) => {
+    setStatus(e.target.value);
+    setFormData((prevData) => ({
+      ...prevData,
+      status: e.target.value
+    }));
+  };
+
+  const handleCountChange = (title, count) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      equipment: {
+        ...prevData.equipment,
+        [title]: count
+      }
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const equipmentArray = [
+      formData.equipment.tables,
+      formData.equipment.chairs,
+      formData.equipment.bulletinBoards,
+      formData.equipment.speaker,
+      formData.equipment.microphone,
+      formData.equipment.flagpole,
+      formData.equipment.podium,
+      formData.equipment.platform,
+      formData.equipment.electrician,
+      formData.equipment.janitor
+  ];
+    
+    const finalFormData = { ...formData, equipment: equipmentArray };
+
+    console.log('Server response:', finalFormData);
+
+    try {
+      const response = await axios.post(`/facify/venue-booking/${orgID}/${facilityID}/create`, finalFormData);
+      console.log('Server response1:', response.data);
+      if (response.data.success) {
+        alert('Booking created successfully');
+        navigate(`/venue-availability/${orgID}`);
+      } else {
+        alert('Error creating booking');
+      }
+    } catch (error) {
+      console.error('Error creating booking:', error);
+      alert('Error creating booking');
+    }
+
+
+  };
+
+  const handleBackButtonClick = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="VenueBooking">
       <Header />
@@ -16,39 +112,33 @@ function VenueBooking() {
           <Sidebar />
         </div>
         <div className="booking-details">
+          <form onSubmit={handleSubmit}>
             <div className="booking-details_header">
                 <div className="booking">
-                    <BackIcon className="back-icon" style={{  fontSize: 40  }}></BackIcon>
+                    <BackIcon className="back-icon" style={{  fontSize: 40  }} onClick={handleBackButtonClick}/>
                     <h2>Multipurpose Hall</h2>
-                    <h3>December 5, 2024 12:00 NN - 4:00 PM</h3>
                 </div>
             </div>
             <div className="venue-input">
                 <h3>Event Information</h3>
                 <div className="content">
                     <div className="venueInfo">
-                        <form>
-                            <label for="org-name">Name of Organization/Department/College:</label>
-                            <input type="text" id="org-name" name="org-name"></input>
+                            <label for="event-date">Date of Activity:</label>
+                            <input type="date" id="event-date" name="eventDate" value={formData.eventDate} onChange={handleChange}></input>
                             <label for="event-start">Start of Activity:</label>
-                            <input type="datetime-local" id="event-start" name="event-start"></input>
-                        </form>
-                    </div>
+                            <input type="time" id="event-start" name="eventStart" value={formData.eventStart} onChange={handleChange}></input>
+                            </div>
                     <div className="venueInfo">
-                        <form>
-                            <label for="event-name">Title of Activity:</label>
-                            <input type="text" id="event-name" name="event-name"></input>
+                            <label for="activity-title">Title of Activity:</label>
+                            <input type="text" id="activity-title" name="activityTitle" value={formData.activityTitle} onChange={handleChange}></input>
                             <label for="event-end">End of Activity:</label>
-                            <input type="datetime-local" id="event-end" name="event-end"></input>
-                        </form>
-                    </div>
+                            <input type="time" id="event-end" name="eventEnd" value={formData.eventEnd} onChange={handleChange}></input>
+                            </div>
                     <div className="venueInfo">
-                        <form>
                             <label for="attendance">Expected Attendance:</label>
-                            <input type="number" id="attendance" name="attendance"></input>
+                            <input type="number" id="attendance" name="attendance" value={formData.attendance} onChange={handleChange}></input>
                             <label for="speaker-name">Name of Speaker (if applicable):</label>
-                            <input type="text" id="speaker-name" name="speaker-name"></input>
-                        </form>
+                            <input type="text" id="speaker-name" name="speakerName" value={formData.speakerName} onChange={handleChange}></input>  
                     </div>
                 </div>
             </div>
@@ -64,11 +154,11 @@ function VenueBooking() {
                     <h4>Microphone</h4>
                   </div>
                   <div className="values">
-                    <AddMinusButton />
-                    <AddMinusButton />
-                    <AddMinusButton />
-                    <AddMinusButton />
-                    <AddMinusButton />
+                    <AddMinusButton item="tables" onCountChange={handleCountChange} />
+                    <AddMinusButton item="chairs" onCountChange={handleCountChange} />
+                    <AddMinusButton item="bulletinBoards" onCountChange={handleCountChange} />
+                    <AddMinusButton item="speaker" onCountChange={handleCountChange} />
+                    <AddMinusButton item="microphone" onCountChange={handleCountChange} />
                   </div>
                 </div>
                 <div className="equipment-column">
@@ -80,11 +170,11 @@ function VenueBooking() {
                     <h4>Janitor</h4>
                   </div>
                   <div className="values">
-                    <AddMinusButton />
-                    <AddMinusButton />
-                    <AddMinusButton />
-                    <AddMinusButton />
-                    <AddMinusButton />
+                    <AddMinusButton item="flagpole" onCountChange={handleCountChange} />
+                    <AddMinusButton item="podium" onCountChange={handleCountChange} />
+                    <AddMinusButton item="platform" onCountChange={handleCountChange} />
+                    <AddMinusButton item="electrician" onCountChange={handleCountChange} />
+                    <AddMinusButton item="janitor" onCountChange={handleCountChange} />
                   </div>
                 </div>
               </div>
@@ -94,25 +184,26 @@ function VenueBooking() {
               <div className="content">
                 <div className="status-column">
                   <label className="form-control">
-                  <input type="radio" id="pencil-booking" name="status"></input>
+                  <input type="radio" id="pencil-booking" name="status" value="pencil" onChange={handleStatusChange}></input>
                   Pencil Booking
                   </label>
                   <label className="form-control">
-                  <input type="radio" id="official-booking" name="status"></input>
-                  Official Booking
+                  <input type="radio" id="complete-reqs" name="status" value="complete" onChange={handleStatusChange}></input>
+                  Complete Requirements
                   </label>    
                 </div>
               </div>
             </div>
             <div className="confirm-booking">
               <label className="agree-checkbox">
-                <input type="checkbox" id="agree-checkbox" name="agree-checkbox"></input>
+                <input type="checkbox" id="agree-checkbox" name="agree-checkbox" required></input>
                 By submitting this reservation, I agree and abide by the rules setforth by the Facilities Management Team.
               </label>
-              <button className="submit-booking">Submit Booking</button>
+              <button className="submit-booking" type="submit">Submit Booking</button>
             </div>
+            </form>
           </div>
-        </div>
+        </div> 
       </div>
   );
 }
