f28282082bf5d4290a9b75a2eea3ee66017b132b
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-02-19 21:13:10 +0800

    Set currentStep to pass as a prop

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -3,24 +3,23 @@ import Progress from './Progress.jsx';
 import './ProgressBar.css';
 import CheckIcon from '@mui/icons-material/Check';
 
-const ProgressBar = () => {
-const [step, setStep] = React.useState(1);
+const ProgressBar = ({ currentStep }) => {
 const totalSteps = 4;
 
   return (
     <div className="container">
     <div className="progress-container">
-      <Progress totalSteps={totalSteps} step={step} className="progress active"/>
-      <div className={`${step>=1 ? "circle active": "circle"}`}><CheckIcon /></div>
-      <div className={`${step>=2 ? "circle active": "circle"}`}><CheckIcon /></div>
-      <div className={`${step>=3 ? "circle active": "circle"}`}><CheckIcon /></div>
-      <div className={`${step>=4 ? "circle active": "circle"}`}><CheckIcon /></div>
+      <Progress totalSteps={totalSteps} step={currentStep} className="progress active"/>
+      <div className={`${currentStep>=1 ? "circle active": "circle"}`}><CheckIcon /></div>
+      <div className={`${currentStep>=2 ? "circle active": "circle"}`}><CheckIcon /></div>
+      <div className={`${currentStep>=3 ? "circle active": "circle"}`}><CheckIcon /></div>
+      <div className={`${currentStep>=4 ? "circle active": "circle"}`}><CheckIcon /></div>
     </div>
     <div className="step-labels">
-      <div className={`${step>=1 ? "step-label active": "step-label"}`}>Pencil Booked</div>
-      <div className={`${step>=2 ? "step-label active": "step-label"}`}>Complete Requirements</div>
-      <div className={`${step>=3 ? "step-label active": "step-label"}`}>For Assessing</div>
-      <div className={`${step>=4 ? "step-label active": "step-label"}`}>Approved</div>
+      <div className={`${currentStep>=1 ? "step-label active": "step-label"}`}>Pencil Booked</div>
+      <div className={`${currentStep>=2 ? "step-label active": "step-label"}`}>Complete Requirements</div>
+      <div className={`${currentStep>=3 ? "step-label active": "step-label"}`}>For Assessing</div>
+      <div className={`${currentStep>=4 ? "step-label active": "step-label"}`}>Approved</div>
     </div>
   </div>
   );
