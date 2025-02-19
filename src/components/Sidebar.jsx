e33e2abf7a4319bf1f5f2374af3f36d27f5b1578
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-02-19 19:48:09 +0800

    Updated sidebar logic, default highlighted tab

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import '../App.css';
 import logo from '../assets/girlypops-pink.png';
 import { SidebarData } from './SidebarData';
 
+
 function Sidebar() {
+
+    const navigate = useNavigate();
+    const { orgID } = useParams();
+
+    const handleNavigation = (path) => {
+        navigate(`${path}/${orgID}`);
+    };
+
     return (
     <div className="Sidebar">
         <h2>MAIN MENU</h2>
         <ul className='SidebarList'>
         {SidebarData.map((val, key)=> {
-        return <li key={key} 
-        className={window.location.pathname === val.path ? "row active" : "row"}
-        onClick={() => {window.location.pathname = val.path}}>
+        return <li 
+        key={key} 
+        className={window.location.pathname.includes(val.path) ? "row active" : "row"} 
+        onClick={() => handleNavigation(val.path)}>
             {" "} 
             <div id="icon">{val.icon}</div>{" "}
             <div id="title">
