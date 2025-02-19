b55c94314af5fe8f3821131dbcb771e1e13e15d7
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-02-19 19:43:42 +0800

    New increment and decrement logic

--- src/components/AddMinusButton.jsx
+++ src/components/AddMinusButton.jsx
@@ -3,15 +3,19 @@ import { useState } from 'react';
 import '../App.css';
 import './AddMinusButton.css';
 
-function AddMinusButton() {
+function AddMinusButton({ item, onCountChange }) {
     const [count, setCount] = useState(0);
 
     const increment = () => {
-        setCount(count + 1);
+        const newCount = count + 1;
+        setCount(newCount);
+        onCountChange(item, newCount);
     }
 
     const decrement = () => {
-        setCount(count - 1);
+        const newCount = count > 0 ? count - 1 : 0;
+        setCount(newCount);
+        onCountChange(item, newCount);
     }
 
     return (
