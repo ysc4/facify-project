207319f91d307c4c4efd5070e78dc13ed7c442ef
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-02-24 16:36:47 +0800

    Added new content

--- src/pages/modals/Condition.jsx
+++ src/pages/modals/Condition.jsx
@@ -17,7 +17,7 @@ const ConditionModal = ({ isOpen, onClose }) => {
           <li><strong>Acceptance of Terms</strong>
           <p>By accessing or using facify, you agree to be bound by these Terms and Conditions of Use. If you do not agree to these Terms, please do not use facify at all and by any means.</p> </li>
           <li><strong>Use of facify</strong>
-          <p>facify is a web-based platform designed for booking facilities at National University - Manila. facify is intended for use by authorized members of the National University - Manila community only. You may use facify only for lawful purposes related to academic, research, or university-sanctioned activities and in accordance with these Terms.</p> </li>
+          <p>facify is a web-based platform designed for facility reservations at National University - Manila. facify is intended for use by authorized members of the National University - Manila community only. You may use facify only for lawful purposes related to academic, research, or university-sanctioned activities and in accordance with these Terms.</p> </li>
           <li><strong>Account Access and Security</strong>
             <ul>
               <li><strong>Authentication:</strong> Users (both students and administrators) must log in using their official University email addresses (student email for students, work email for administrators).</li>
@@ -38,6 +38,55 @@ const ConditionModal = ({ isOpen, onClose }) => {
               <li>Use facify for any commercial purposes or for any activity that is not directly related to University-sanctioned events or activities.</li>
               <li>Share your account credentials with any other person.</li>
             </ul>
+          </li>
+          <li><strong>Facility Reservation Procedures</strong>
+            <ul>
+              <li><strong>User Requirements:</strong> Users/Students are required to read and agree to the Terms and Conditions for Facility Use before submitting a facility reservation:
+              <ul>
+                <li>Submit the reservation form along with the venue layout.</li>
+                <li>Use the facility only for the purpose stated in the reservation form.</li>
+                <li>Decorations must comply with guidelines and avoid fire safety equipment or structural elements.</li>
+                <li>Posting materials on walls or backdrops is strictly prohibited.</li>
+                <li>Organizations are responsible for setup, cleanup, and restoring the facility to its original state.</li>
+                <li>Ensure compliance with facility capacity limits.</li>
+                <li>Alcohol and drugs are prohibited on the premises.</li>
+                <li>Individuals under the influence are not allowed.</li>
+                <li>Vacate the facility after the reserved date and time.</li>
+                <li>Conduct final coordination with the Physical Facilities Management Office staff three to four days before the event.</li>
+              </ul>
+              </li>
+              <li><strong>Requirement Submission:</strong> Students are required to submit the soft copy of the following documents within <strong>two weeks</strong> of submitting their facility reservation:
+              <ul>
+                <li>Activity Request Form</li>
+                <li>Event Proposal</li>
+                <li>Letter of Intent</li>
+                <li>Ingress Report</li>
+              </ul>
+              <p>These documents must be uploaded to the "Submitted Requirements" section within the "Facility Reservation" tab menu.</p>
+              </li>
+              <li><strong>Facility Reservation Assessment Phase:</strong> Upon the completion of all required document submissions by the student, the facility reservation will enter its assessment phase.</li>
+              <li><strong>Facility Reservation Cancellation/Editing:</strong> Students may cancel or edit their facility reservation before the assessment phase ("For Assessing") begins.</li>
+              <li><strong>Event Information Visibility at the Facility Availability Calendar:</strong> 
+              <ul>
+                <li>Students can only view brief or main details of events on the public "Facility Availability" calendar to prevent any event spoilers or leaks.
+                  <ul>
+                    <li>Students can fully view their own submitted event information, facility reservation/booking information, and submitted requirements (including Update Log) within their "Facility Reservation" tab.</li>
+                  </ul>
+                </li>
+                <li>Full event details are accessible only to administrators on their administrative side of the Facility Availability calendar.</li>
+              </ul>
+              </li>
+              <li><strong>Facility Reservation Approval/Denial:</strong> Administrators have the authority to approve or deny facility reservations. Students will be notified of the assessment outcome (approved or denied) of their facility reservations through the "Update Log" section within their "Facility Reservation" tab.</li>
+            </ul>
+          </li>
+          <li><strong>Disclaimer of Warranties</strong>
+          <p>facify is provided "as is" without any warranties, express or implied, including but not limited to warranties of merchantability for a particular purpose. <strong>We do not warrant that facify will be uninterrupted or error-free.</strong></p>
+          </li>
+          <li><strong>Changes to Terms</strong>
+          <p>Should there be any important updates or changes to the website, the project team (girlypops) will update these Terms occasionally. We will notify users of any material changes by posting the updated Terms on this page.</p>
+          </li>
+          <li><strong>Governing Law</strong>
+          <p>These Terms and Conditions shall be governed by and construed in accordance with the laws of the Philippines.</p>
           </li>
         </ol>
       </div>
