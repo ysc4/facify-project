fd7c054bc2b258fcbfc4dd5bf19807945e1b058b
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-02-26 18:49:18 +0800

    Initialized utils to be used globally

---
+++ src/utils/StatusUtil.jsx
@@ -0,0 +1,11 @@
+export const getStatusColor = (status) => {
+    switch (status) {
+        case "Pencil Booked": return "#FFF3B4";
+        case "Officially Booked": return "#A6C4FF";
+        case "For Assessing": return "#FFB951";
+        case "Approved": return "#B3FFA6";
+        case "Denied": return "#FFA6A6";
+        case "Cancelled": return "#D9D9D9";
+        default: return "#FFFFFF"; 
+    }
+};
