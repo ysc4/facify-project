ab6e791ebcb80bda43b4748cb882008ae8fefd50
Jessy Cassandra <jessy.mapanao03@gmail.com>
2025-02-27 11:19:50 +0800

    Updated getStatusColor

--- src/utils/StatusUtil.jsx
+++ src/utils/StatusUtil.jsx
@@ -6,6 +6,8 @@ export const getStatusColor = (status) => {
         case "Approved": return "#B3FFA6";
         case "Denied": return "#FFA6A6";
         case "Cancelled": return "#D9D9D9";
+        case "Total Bookings": return "#262323";
+        case "Scheduled Today": return "#FFB8D6";
         default: return "#FFFFFF"; 
     }
 };
